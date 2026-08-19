6752a9fce259113f29fc196f491580d8f0b76ec4
mtkmk1881 <mtkmk1881@gmail.com>
2026-08-19 19:13:04 +0300

    Implement asynchronous mail sending with Tokio and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+mod services;
 
+use services::mail::send;
+use std::error::Error;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let result = send().await;
+    println!("Result: {:?}", result);
+
+    Ok(())
 }
--- src/services/mail.rs
+++ src/services/mail.rs
@@ -1,6 +1,21 @@
 use std::error::Error;
-use send_mail;
+use mail_send::{mail_builder::MessageBuilder, SmtpClientBuilder};
 
-pub fn send() -> Result<(), dyn Error>{
+pub async fn send() -> Result<(), Box<dyn Error>> {
+    let message = MessageBuilder::new()
+        .from(("Mustafa Tkm", "mtkmk1881@gmail.com"))
+        .to("mtkmk1881@gmail.com")
+        .subject("Naber Cınım")
+        .text_body("Nasılsın ?\r\n")
+        .html_body("<p>iyi misin ?</p>");
 
+    SmtpClientBuilder::new("smtp.gmail.com", 587)?
+        .implicit_tls(false)
+        .credentials(("mtkmk1881@gmail.com", "esgu bafl akfv bqzd"))
+        .connect()
+        .await?
+        .send(message)
+        .await?;
+
+    Ok(())
 }
