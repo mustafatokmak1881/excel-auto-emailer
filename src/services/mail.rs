d5c81596ffbdc55e929bdd92ca0ad5ecbb51cafa
mtkmk1881 <mtkmk1881@gmail.com>
2026-08-19 19:16:44 +0300

    Refactor email handling to use a variable for the sender's email address

--- src/services/mail.rs
+++ src/services/mail.rs
@@ -2,16 +2,17 @@ use std::error::Error;
 use mail_send::{mail_builder::MessageBuilder, SmtpClientBuilder};
 
 pub async fn send() -> Result<(), Box<dyn Error>> {
+    let my_email: &str = "mtkmk1881@gmail.com";
     let message = MessageBuilder::new()
-        .from(("Mustafa Tkm", "mtkmk1881@gmail.com"))
-        .to("mtkmk1881@gmail.com")
-        .subject("Naber Cınım")
+        .from(("Mustafa Tkm", my_email))
+        .to(my_email)
+        .subject("Derken patır kütür ya şş")
         .text_body("Nasılsın ?\r\n")
         .html_body("<p>iyi misin ?</p>");
 
     SmtpClientBuilder::new("smtp.gmail.com", 587)?
         .implicit_tls(false)
-        .credentials(("mtkmk1881@gmail.com", "esgu bafl akfv bqzd"))
+        .credentials((my_email, "esgu bafl akfv bqzd"))
         .connect()
         .await?
         .send(message)
