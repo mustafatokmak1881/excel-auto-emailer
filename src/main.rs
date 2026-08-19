80936d9a0b80d8e826c17187209d2b09ddb015b8
mtkmk1881 <mtkmk1881@gmail.com>
2026-08-19 19:54:32 +0300

    Comment out service module and email sending code in main function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-mod services;
+//mod services;
 
-use services::mail::send;
+//use services::mail::send;
 use std::error::Error;
+use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let result = send("mtkmk1881@gmail.com", "MErhabaaaa", "<marquee>Html Alanı buraya ya şş</marquee>").await;
-    println!("Result: {:?}", result);
+    let paths =  fs::read_dir(".")?;
+    for path in paths {
+        println!("dir: {}", path?.path().display());
+    }
+
+    //let result = send("mtkmk1881@gmail.com", "M3rhabaaaa", "<html style='background-color:grey'>Html Alanı buraya ya şş</html>").await;
+    //println!("Result: {:?}", result);
 
     Ok(())
 }
