0ed663081e75ccc08ef59dbd55358cec524dee86
mtkmk1881 <mtkmk1881@gmail.com>
2026-08-21 13:36:08 +0300

    Update dependencies and refactor main function to create directory for Excel files

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@
 
 //use services::mail::send;
 use std::error::Error;
+// use std::env;
+use std::path::Path;
 use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let paths =  fs::read_dir(".")?;
+    let my_dir : &str = "excelfilesx";
+
+    let exists: bool = Path::new(my_dir).is_dir();
+    if !exists {
+        fs::create_dir(my_dir);
+    }
+
+    let paths = fs::read_dir(my_dir)?;
     for path in paths {
-        println!("dir: {}", path?.path().display());
+        let item: String = path?.path().display().to_string();
+        println!("dir: {}", item);
     }
 
     //let result = send("mtkmk1881@gmail.com", "M3rhabaaaa", "<html style='background-color:grey'>Html Alanı buraya ya şş</html>").await;
